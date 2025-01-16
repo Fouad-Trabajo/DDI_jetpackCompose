877e69b74339e64740fa003fed3102292791e399
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2025-01-16 18:59:55 +0100

    feat: added image

--- app/src/main/java/com/example/tutorialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/tutorialjetpackcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 
 import androidx.compose.ui.text.font.FontFamily
@@ -58,6 +60,7 @@ fun preview() {
             matrixIdentity(5)
         }
         mesage("mundo!")
+        Image(painterResource(R.drawable.ic_launcher_foreground), contentDescription = "Artist image")
     }
 }
 
@@ -96,6 +99,7 @@ fun mesage(name: String) {
         //fontFamily = FontFamily(Font(R.font.bhutuka_expanded_one)),
         modifier= Modifier.horizontalScroll(rememberScrollState(0)
         )
+
     )
 }
 
